9d0da2605b29cdf227ca6f53637fc29c7b5a7c3e
drakontia <mhden@drakontia.com>
2014-03-04 17:54:23 +0900

    Dont read Gemfile.local by instance_eval

--- redmine_install.sh
+++ redmine_install.sh
@@ -89,6 +89,8 @@ ln -sf $PATH_TO_PLUGIN $PATH_TO_INSTALL/$NAME_OF_PLUGIN
 # copy database.yml
 cp $TESTSPACE/database.yml config/
 
+cp $PATH_TO_INSTALL/$NAME_OF_PLUGIN/Gemfile.local $PATH_TO_REDMINE/
+
 # install gems
 mkdir -p vendor/bundle
 bundle install --path vendor/bundle
