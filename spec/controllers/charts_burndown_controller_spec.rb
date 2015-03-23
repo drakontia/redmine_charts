f1ad5d862e36d24086665a72908a5720229c1621
drakontia <mhden@drakontia.com>
2015-03-23 19:21:54 +0900

    Modify burndown_controller for Rspec3

--- spec/controllers/charts_burndown_controller_spec.rb
+++ spec/controllers/charts_burndown_controller_spec.rb
@@ -1,103 +1,102 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../rails_helper'
 
-describe ChartsBurndownController do
-
+Rspec.describe ChartsBurndownController, type: :controller do
+  describe 'GET #index' do
   include Redmine::I18n
 
   before do
-    Time.set_current_date = Time.mktime(2010,3,12)
+    Time.set_current_date = Time.mktime(2010, 3, 12)
     @controller = ChartsBurndownController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     Setting.default_language = 'en'
-    @request.session['user_id'] = 1
   end
 
-  it "should return data with range days limit 4 offset 1" do
-    get :index, :project_id => 15041, :project_ids => [15041, 15042], :limit => '4', :range => 'days', :offset => '1'
-    response.should be_success
+  it 'return data with range days limit 4 offset 1' do
+    get :index, project_id: 15041, project_ids: [15041, 15042], limit: '4', range: 'days', offset: '1'
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
 
-    body = ActiveSupport::JSON.decode(assigns[:data])
-    body['y_legend']['text'].should == l(:charts_burndown_y)
-    body['x_legend']['text'].should == l(:charts_burndown_x)
-    body['y_axis']['max'].should be_close(81, 1)
-    body['y_axis']['min'].should == 0
-    body['x_axis']['max'].should be_close(4, 1)
-    body['x_axis']['min'].should == 0
-    body['x_axis']['labels']['labels'].size.should == 4
-    body['x_axis']['labels']['labels'][0].should == '08 Mar 10'
-    body['x_axis']['labels']['labels'][1].should == '09 Mar 10'
-    body['x_axis']['labels']['labels'][2].should == '10 Mar 10'
-    body['x_axis']['labels']['labels'][3].should == '11 Mar 10'
+    #body = ActiveSupport::JSON.decode(assigns[:data])
+    expect(assigns(:data).y_legend.text).to eq l(:charts_burndown_y)
+    expect(assigns(:data).x_legend.text).to eq l(:charts_burndown_x)
+    expect(assigns(:data).y_axis.max).to be_within(81, 1)
+    expect(assigns(:data).y_axis.min).to eq 0
+    expect(assigns(:data).x_axis.max).to be_within(4, 1)
+    expect(assigns(:data).x_axis.min).to eq 0
+    expect(assigns(:data).x_axis.labels.labels.size).to eq 4
+    expect(assigns(:data).x_axis.labels.labels[0]).to eq '08 Mar 10'
+    expect(assigns(:data).x_axis.labels.labels[1]).to eq '09 Mar 10'
+    expect(assigns(:data).x_axis.labels.labels[2]).to eq '10 Mar 10'
+    expect(assigns(:data).x_axis.labels.labels[3]).to eq '11 Mar 10'
 
-    body['elements'].size.should == 4
-    body['elements'][0]['values'].size.should == 4
-    body['elements'][0]['text'].should == l(:charts_burndown_group_estimated)
-    body['elements'][1]['values'].size.should == 4
-    body['elements'][1]['text'].should == l(:charts_burndown_group_logged)
-    body['elements'][2]['values'].size.should == 4
-    body['elements'][2]['text'].should == l(:charts_burndown_group_remaining)
-    body['elements'][3]['values'].size.should == 4
-    body['elements'][3]['text'].should == l(:charts_burndown_group_predicted)
+    expect(assigns(:data).elements.size).to eq 4
+    expect(assigns(:data).elements[0].values.size).to eq 4
+    expect(assigns(:data).elements[0].text).to eq l(:charts_burndown_group_estimated)
+    expect(assigns(:data).elements[1].values.size).to eq 4
+    expect(assigns(:data).elements[1].text).to eq l(:charts_burndown_group_logged)
+    expect(assigns(:data).elements[2].values.size).to eq 4
+    expect(assigns(:data).elements[2].text).to eq l(:charts_burndown_group_remaining)
+    expect(assigns(:data).elements[3].values.size).to eq 4
+    expect(assigns(:data).elements[3].text).to eq l(:charts_burndown_group_predicted)
 
-    body['elements'][0]['values'][0]['value'].should be_close(23,0.1)
-    body['elements'][0]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_estimated, :estimated_hours => 23.0)}<br>#{'08 Mar 10'}"
-    body['elements'][0]['values'][1]['value'].should be_close(35,0.1)
-    body['elements'][0]['values'][1]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>#{'09 Mar 10'}"
-    body['elements'][0]['values'][2]['value'].should be_close(35,0.1)
-    body['elements'][0]['values'][2]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>#{'10 Mar 10'}"
-    body['elements'][0]['values'][3]['value'].should be_close(35,0.1)
-    body['elements'][0]['values'][3]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>#{'11 Mar 10'}"
+    expect(assigns(:data).elements[0].values[0].value).to be_within(0[1]).of(23)
+    expect(assigns(:data).elements[0].values[0].tip).to eq "#{l(:charts_burndown_hint_estimated, :estimated_hours => 23.0)}<br>08 Mar 10"
+    expect(assigns(:data).elements[0].values[1].value).to be_within(0.1).of(35)
+    expect(assigns(:data).elements[0].values[1].tip).to eq "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>09 Mar 10"
+    expect(assigns(:data).elements[0].values[2].value).to be_within(35,0.1)
+    expect(assigns(:data).elements[0].values[2].tip).to eq "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>10 Mar 10"
+    expect(assigns(:data).elements[0].values[3].value).to be_within(35,0.1)
+    expect(assigns(:data).elements[0].values[3].tip).to eq "#{l(:charts_burndown_hint_estimated, :estimated_hours => 35.0)}<br>11 Mar 10"
 
-    body['elements'][1]['values'][0]['value'].should be_close(29.35,0.1)
-    body['elements'][1]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_logged, :logged_hours => 29.4)}<br>#{'08 Mar 10'}"
-    body['elements'][1]['values'][1]['value'].should be_close(35.95,0.1)
-    body['elements'][1]['values'][1]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_logged, :logged_hours => 36.0)}<br>#{'09 Mar 10'}"
-    body['elements'][1]['values'][2]['value'].should be_close(43.35,0.1)
-    body['elements'][1]['values'][2]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_logged, :logged_hours => 43.4)}<br>#{'10 Mar 10'}"
-    body['elements'][1]['values'][3]['value'].should be_close(43.35,0.1)
-    body['elements'][1]['values'][3]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_logged, :logged_hours => 43.4)}<br>#{'11 Mar 10'}"
+    expect(assigns(:data).elements[1].values[0].value).to be_within(29.35, 0.1)
+    expect(assigns(:data).elements[1].values[0].tip).to eq "#{l(:charts_burndown_hint_logged, :logged_hours => 29.4)}<br>08 Mar 10"
+    expect(assigns(:data).elements[1].values[1].value).to be_within(35.95,0.1)
+    expect(assigns(:data).elements[1].values[1].tip).to eq "#{l(:charts_burndown_hint_logged, :logged_hours => 36.0)}<br>09 Mar 10"
+    expect(assigns(:data).elements[1].values[2].value).to be_within(43.35,0.1)
+    expect(assigns(:data).elements[1].values[2].tip).to eq "#{l(:charts_burndown_hint_logged, :logged_hours => 43.4)}<br>10 Mar 10"
+    expect(assigns(:data).elements[1].values[3].value).to be_within(43.35,0.1)
+    expect(assigns(:data).elements[1].values[3].tip).to eq "#{l(:charts_burndown_hint_logged, :logged_hours => 43.4)}<br>11 Mar 10"
 
-    body['elements'][2]['values'][0]['value'].should be_close(5.1,0.1)
-    body['elements'][2]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_remaining, :remaining_hours => 5.1, :work_done => 90)}<br>#{'08 Mar 10'}"
-    body['elements'][2]['values'][1]['value'].should be_close(31.5,0.1)
-    body['elements'][2]['values'][1]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_remaining, :remaining_hours => 31.5, :work_done => 76)}<br>#{'09 Mar 10'}"
-    body['elements'][2]['values'][2]['value'].should be_close(11.0,0.1)
-    body['elements'][2]['values'][2]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_remaining, :remaining_hours => 11.0, :work_done => 84)}<br>#{'10 Mar 10'}"
-    body['elements'][2]['values'][3]['value'].should be_close(11.0,0.1)
-    body['elements'][2]['values'][3]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_remaining, :remaining_hours => 11.0, :work_done => 84)}<br>#{'11 Mar 10'}"
+    expect(assigns(:data).elements[2].values[0].value).to be_within(5.1,0.1)
+    expect(assigns(:data).elements[2].values[0].tip).to eq "#{l(:charts_burndown_hint_remaining, :remaining_hours => 5.1, :work_done => 90)}<br>08 Mar 10"
+    expect(assigns(:data).elements[2].values[1].value).to be_within(31.5,0.1)
+    expect(assigns(:data).elements[2].values[1].tip).to eq "#{l(:charts_burndown_hint_remaining, :remaining_hours => 31.5, :work_done => 76)}<br>09 Mar 10"
+    expect(assigns(:data).elements[2].values[2].value).to be_within(11.0,0.1)
+    expect(assigns(:data).elements[2].values[2].tip).to eq "#{l(:charts_burndown_hint_remaining, :remaining_hours => 11.0, :work_done => 84)}<br>10 Mar 10"
+    expect(assigns(:data).elements[2].values[3].value).to be_within(11.0,0.1)
+    expect(assigns(:data).elements[2].values[3].tip).to eq "#{l(:charts_burndown_hint_remaining, :remaining_hours => 11.0, :work_done => 84)}<br>11 Mar 10"
 
-    body['elements'][3]['values'][0]['value'].should be_close(34.4,0.1)
-    body['elements'][3]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 34.5, :hours_over_estimation => 11.5)}<br>#{'08 Mar 10'}"
-    body['elements'][3]['values'][1]['value'].should be_close(67.4,0.1)
-    body['elements'][3]['values'][1]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 67.5, :hours_over_estimation => 32.5)}<br>#{'09 Mar 10'}"
-    body['elements'][3]['values'][2]['value'].should be_close(54.4,0.1)
-    body['elements'][3]['values'][2]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 54.4, :hours_over_estimation => 19.4)}<br>#{'10 Mar 10'}"
-    body['elements'][3]['values'][3]['value'].should be_close(54.4,0.1)
-    body['elements'][3]['values'][3]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 54.4, :hours_over_estimation => 19.4)}<br>#{'11 Mar 10'}"
+    expect(assigns(:data).elements[3].values[0].value).to be_within(34.4,0.1)
+    expect(assigns(:data).elements[3].values[0].tip).to eq "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 34.5, :hours_over_estimation => 11.5)}<br>08 Mar 10"
+    expect(assigns(:data).elements[3].values[1].value).to be_within(67.4,0.1)
+    expect(assigns(:data).elements[3].values[1].tip).to match(/#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 67.5, :hours_over_estimation => 32.5)}<br>09 Mar 10/)
+    expect(assigns(:data).elements[3].values[2].value).to be_within(54.4,0.1)
+    expect(assigns(:data).elements[3].values[2].tip).to match(/#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 54.4, :hours_over_estimation => 19.4)}<br>10 Mar 10/)
+    expect(assigns(:data).elements[3].values[3].value).to be_within(54.4,0.1)
+    expect(assigns(:data).elements[3].values[3].tip).to match(/#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 54.4, :hours_over_estimation => 19.4)}<br>11 Mar 10/)
 
   end
 
-  it "should return data with range weeks limit 1 if it has sub_tasks" do
+  it 'should return data with range weeks limit 1 if it has sub_tasks' do
     if RedmineCharts.has_sub_issues_functionality_active
-      get :index, :project_id => 15044, :project_ids => [15044], :limit => '1', :range => 'weeks', :offset => '0'
+      get :index, project_id: 15044, project_ids: [15044], limit: 1, range: 'weeks', offset: 0
       response.should be_success
 
-      body = ActiveSupport::JSON.decode(assigns[:data])
-      body['elements'][0]['values'].size.should == 1
+      expect(assigns(:data).elements[0].values.size).to eq 1
 
-      body['elements'][0]['values'][0]['value'].should be_close(12,0.1)
-      body['elements'][0]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_estimated, :estimated_hours => 12.0)}<br>#{'8 - 14 Mar 10'}"
+      expect(assigns(:data).elements[0].values[0].value).to be_within(12,0.1)
+      expect(assigns(:data).elements[0].values[0].tip).to match(/#{l(:charts_burndown_hint_estimated, :estimated_hours => 12.0)}<br>8 - 14 Mar 10/)
 
-      body['elements'][1]['values'][0]['value'].should be_close(13.2,0.1)
-      body['elements'][1]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_logged, :logged_hours => 13.2)}<br>#{'8 - 14 Mar 10'}"
+      expect(assigns(:data).elements[1].values[0].value).to be_within(13.2,0.1)
+      expect(assigns(:data).elements[1].values[0].tip).to match(/#{l(:charts_burndown_hint_logged, :logged_hours => 13.2)}<br>8 - 14 Mar 10/)
 
-      body['elements'][2]['values'][0]['value'].should be_close(12,0.1)
-      body['elements'][2]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_remaining, :remaining_hours => 12.0, :work_done => 0)}<br>#{'8 - 14 Mar 10'}"
+      expect(assigns(:data).elements[2].values[0].value).to be_within(12,0.1)
+      expect(assigns(:data).elements[2].values[0].tip).to match(/#{l(:charts_burndown_hint_remaining, :remaining_hours => 12.0, :work_done => 0)}<br>8 - 14 Mar 10/)
 
-      body['elements'][3]['values'][0]['value'].should be_close(25.2,0.1)
-      body['elements'][3]['values'][0]['tip'].gsub("\\u003C", "<").gsub("\\u003E", ">").gsub("\000", "").should == "#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 25.2, :hours_over_estimation => 13.2)}<br>#{'8 - 14 Mar 10'}"
+      expect(assigns(:data).elements[3].values[0].value).to be_within(25[2],0[1])
+      expect(assigns(:data).elements[3].values[0].tip).to match(/#{l(:charts_burndown_hint_predicted_over_estimation, :predicted_hours => 25.2, :hours_over_estimation => 13.2)}<br>8 - 14 Mar 10/)
     end
   end
-
+  end
 end
