9a4712505a20209c9c9816eec014c3855759ae34
drakontia <mhden@drakontia.com>
2015-03-24 19:08:48 +0900

    Require Open Flash Chart lib

--- init.rb
+++ init.rb
@@ -2,30 +2,6 @@ require 'redmine'
 
 require 'redmine_charts'
 
-if Rails::VERSION::MAJOR < 3
-  RAILS_DEFAULT_LOGGER.info 'Starting Charts Plugin for RedMine'
-
-  require 'dispatcher'
-  object_to_prepare = Dispatcher
-else
-  Rails.logger.info 'Starting Charts Plugin for RedMine'
-  object_to_prepare = Rails.configuration
-  # if redmine plugins were railties:
-  # object_to_prepare = config
-end
-object_to_prepare.to_prepare do
-  require_dependency 'issue'
-  require_dependency 'time_entry'
-
-  unless Issue.included_modules.include? RedmineCharts::IssuePatch
-    Issue.send(:include, RedmineCharts::IssuePatch)
-  end
-
-  unless TimeEntry.included_modules.include? RedmineCharts::TimeEntryPatch
-    TimeEntry.send(:include, RedmineCharts::TimeEntryPatch)
-  end
-end
-
 Redmine::Plugin.register :redmine_charts do
   name 'Redmine Charts'
   author 'Daisuke Miura'
