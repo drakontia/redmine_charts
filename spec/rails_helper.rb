f0092fce1714e8d954f64a0ee9872ae9a75652e1
drakontia <mhden@drakontia.com>
2015-03-24 11:20:42 +0900

    Use spec type tag manualy

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,16 +21,6 @@ Spork.prefork do
     config.use_transactional_fixtures = true
     config.global_fixtures = :all
 
-    # rspec-rails 3 will no longer automatically infer an example group's spec type
-    # from the file location. You can explicitly opt-in to the feature using this
-    # config option.
-    # To explicitly tag specs without using automatic inference, set the `:type`
-    # metadata manually:
-    #
-    #     describe ThingsController, :type => :controller do
-    #       # Equivalent to being in spec/controllers
-    #     end
-    config.infer_spec_type_from_file_location!
   end
 end
 
@@ -62,14 +52,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.global_fixtures = :all
 
-  # rspec-rails 3 will no longer automatically infer an example group's spec type
-  # from the file location. You can explicitly opt-in to the feature using this
-  # config option.
-  # To explicitly tag specs without using automatic inference, set the `:type`
-  # metadata manually:
-  #
-  #     describe ThingsController, :type => :controller do
-  #       # Equivalent to being in spec/controllers
-  #     end
-  config.infer_spec_type_from_file_location!
 end
