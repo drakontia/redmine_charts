2a4b5f01fdb816bbf5d7d0b623548dcd76cb54b5
drakontia <mhden@drakontia.com>
2016-08-31 17:51:30 +0900

    Fix routing spec

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes /charts/burndown/index to burndown' do
-    expect(get: '/projects/Project1/charts/burndown/index').to route_to(
+    expect(:get => "/projects/Project1/charts/burndown/index").to route_to(
       controller: 'charts_burndown',
       project_id: 'Project1',
       action: 'index'
@@ -15,7 +15,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes to burndown2' do
-    expect(get: '/projects/Project1/charts/burndown2/index').to route_to(
+    expect(:get => "/projects/Project1/charts/burndown2/index").to route_to(
       controller: 'charts_burndown2',
       project_id: 'Project1',
       action: 'index'
@@ -23,7 +23,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes to ratio' do
-    expect(get: '/projects/Project1/charts/ratio/index').to route_to(
+    expect(:get => "/projects/Project1/charts/ratio/index").to route_to(
       controller: 'charts_ratio',
       project_id: 'Project1',
       action: 'index'
@@ -31,7 +31,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes to timeline' do
-    expect(get: '/projects/Project1/charts/timeline/index').to route_to(
+    expect(:get => "/projects/Project1/charts/timeline/index").to route_to(
       controller: 'charts_timeline',
       project_id: 'Project1',
       action: 'index'
@@ -39,7 +39,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes to deviation' do
-    expect(get: '/projects/Project1/charts/deviation/index').to route_to(
+    expect(:get => "/projects/Project1/charts/deviation/index").to route_to(
       controller: 'charts_deviation',
       project_id: 'Project1',
       action: 'index'
@@ -47,7 +47,7 @@ RSpec.describe 'Routing', type: :routing do
   end
 
   it 'routes to Issue' do
-    expect(get: '/projects/Project1/charts/issue/index').to route_to(
+    expect(:get => "/projects/Project1/charts/issue/index").to route_to(
       controller: 'charts_issue',
       project_id: 'Project1',
       action: 'index'
